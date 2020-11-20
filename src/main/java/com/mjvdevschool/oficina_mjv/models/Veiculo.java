c7a3ce8bc8c3ede47eeac8d48563b6f5289dec63
leonardorochaf <leonardo.rocha0803@gmail.com>
2020-11-20 15:44:08 -0300

    feat: criacao do modelo de veiculo

---
+++ src/main/java/com/mjvdevschool/oficina_mjv/models/Veiculo.java
@@ -0,0 +1,11 @@
+package com.mjvdevschool.oficina_mjv.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Veiculo {
+
+    private Long id;
+    private String nome;
+}
