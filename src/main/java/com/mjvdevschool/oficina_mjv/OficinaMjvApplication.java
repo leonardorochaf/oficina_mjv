ec2229b3c2bc8eda8e90c6252ce5572cae751763
leonardorochaf <leonardo.rocha0803@gmail.com>
2020-11-20 15:23:22 -0300

    chore: adição dependências spring web, spring data jdbc, spring devtools, h2 database, lombok e dependências relacionadas a jsp

---
+++ src/main/java/com/mjvdevschool/oficina_mjv/OficinaMjvApplication.java
@@ -0,0 +1,13 @@
+package com.mjvdevschool.oficina_mjv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinaMjvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OficinaMjvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mjvdevschool/oficina_mjv/OficinaMjvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjvdevschool.oficina_mjv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinaMjvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
