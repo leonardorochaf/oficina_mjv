592ae760c54a02da96126a005552ba185d6c807a
leonardorochaf <leonardo.rocha0803@gmail.com>
2020-11-20 15:38:32 -0300

    feat: criacao do modelo de peca

---
+++ src/main/java/com/mjvdevschool/oficina_mjv/models/Peca.java
@@ -0,0 +1,11 @@
+package com.mjvdevschool.oficina_mjv.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Peca {
+
+    private Long id;
+    private String nome;
+}
