1c8f64ece598d5fb8824dd3c1d65ad9fe06a1d90
leonardorochaf <leonardo.rocha0803@gmail.com>
2020-11-20 16:10:44 -0300

    feat: criacao da pagina de cadastro de peca

---
+++ src/main/resources/static/js/commom/checkboxValidation.js
@@ -0,0 +1,10 @@
+$(function(){
+    var requiredCheckboxes = $('.checkbox-group :checkbox');
+    requiredCheckboxes.change(function(){
+        if(requiredCheckboxes.is(':checked')) {
+            requiredCheckboxes.removeAttr('required');
+        } else {
+            requiredCheckboxes.attr('required', 'required');
+        }
+    });
+});
