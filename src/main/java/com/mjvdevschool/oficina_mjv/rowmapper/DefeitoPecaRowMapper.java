b1f4b2494fd01893fa7e3f92b91e28b1503e6627
leonardorochaf <leonardo.rocha0803@gmail.com>
2020-11-25 10:15:11 -0300

    refactor: mudanca no row mapper para o relacionamento entre defeito e peca

--- src/main/java/com/mjvdevschool/oficina_mjv/rowmapper/DefeitoPecaRowMapper.java
+++ src/main/java/com/mjvdevschool/oficina_mjv/rowmapper/DefeitoPecaRowMapper.java
@@ -1,5 +1,7 @@
 package com.mjvdevschool.oficina_mjv.rowmapper;
 
+import com.mjvdevschool.oficina_mjv.models.Defeito;
+import com.mjvdevschool.oficina_mjv.models.Peca;
 import com.mjvdevschool.oficina_mjv.modelsDTO.DefeitoPecaDTO;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -12,10 +14,17 @@ public class DefeitoPecaRowMapper implements RowMapper<DefeitoPecaDTO> {
         DefeitoPecaDTO defeitoPecaDTO = new DefeitoPecaDTO();
 
         defeitoPecaDTO.setId(rs.getLong(1));
-        defeitoPecaDTO.setIdDefeito(rs.getLong(2));
-        defeitoPecaDTO.setNomeDefeito(rs.getString(3));
-        defeitoPecaDTO.setIdPeca(rs.getLong(4));
-        defeitoPecaDTO.setNomePeca(rs.getString(5));
+
+        Defeito defeito = new Defeito();
+        defeito.setId(rs.getLong(2));
+        defeito.setNome(rs.getString(3));
+
+        Peca peca = new Peca();
+        peca.setId(rs.getLong(4));
+        peca.setNome(rs.getString(5));
+
+        defeitoPecaDTO.setDefeito(defeito);
+        defeitoPecaDTO.setPeca(peca);
 
         return defeitoPecaDTO;
     }
